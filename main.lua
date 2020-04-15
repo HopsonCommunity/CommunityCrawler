0ed939792fb88eee695718ff0ce7d4f5c98774c8
IsLilyYaGirl <35901613+IsLilyYaGirl@users.noreply.github.com>
2020-04-15 21:34:13 +0100

    Shove Shädows in
    stuck a light on the player for testing

--- main.lua
+++ main.lua
@@ -5,6 +5,8 @@ require "player"
 require "mapGenerator"
 require "drawables"
 
+lightworld:SetColor(0, 0, 0)
+
 function love.load()
     player = Player()
     player:load()
@@ -19,11 +21,19 @@ end
 
 function love.update(dt)
     player:update(dt)
+	lightworld:Update()
+	lightworld:SetPosition(player.x * 32 + (-love.graphics.getWidth() / 2), player.y * 32 + (-love.graphics.getHeight() / 2), 1)
+	playerLight:SetPosition((player.x * 32), (player.y * 32))
+end
+
+function love.mousepressed(button, x, y)
+	
 end
 
 function love.draw()
     love.graphics.reset()
     love.graphics.push()
+	
 
     love.graphics.setBackgroundColor(0, 0, 0)
     love.graphics.translate(-player.x * 32 + (love.graphics.getWidth() / 2), -player.y * 32 + (love.graphics.getHeight() / 2))
@@ -41,6 +51,7 @@ function love.draw()
     if player.debugMode then
         drawDebug()
     end
+	lightworld:Draw()
 
     love.graphics.pop()
 end
