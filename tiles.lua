038d828abd5062d5901062bc478d497dbafea058
IsLilyYaGirl <35901613+IsLilyYaGirl@users.noreply.github.com>
2020-04-16 20:32:14 +0100

    tile layering!

--- tiles.lua
+++ tiles.lua
@@ -23,7 +23,14 @@ tiles["halfWall"] = {
 tiles["crate"] = {
     id = "crate",
     solid = true,
-    texture = "crate"
+    texture = "crate",
+	layer = "brickFloor",
+	lightHitbox = {
+		{x = 4, y = 0},
+		{x = 26, y = 0},
+		{x = 26, y = 32},
+		{x = 4, y = 32}
+	}
 }
 
 tiles["lightFloor"] = {
@@ -55,6 +62,11 @@ end
 function fillSpriteBatch(sb, tileMap)
     for k, v in pairs(tileMap) do
         local coords = split(k, " ")
+		if v.layer then
+			local col = atlasOffsets[tiles[v.layer].texture]
+			local quad = love.graphics.newQuad(col * 32, 0, 32, 32, 3200, 32)
+			sb:add(quad, coords[1] * 32, coords[2] * 32)
+		end
 		local col = atlasOffsets[v.texture]
 		local quad = love.graphics.newQuad(col * 32, 0, 32, 32, 3200, 32)
 		sb:add(quad, coords[1] * 32, coords[2] * 32)
