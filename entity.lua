2f918d1dbf91917776d8b4b0f07b984994c59cde
IsLilyYaGirl <35901613+IsLilyYaGirl@users.noreply.github.com>
2020-04-19 21:30:38 +0100

    Fix current issue

---
+++ entity.lua
@@ -0,0 +1,121 @@
+require "libs.SECL.full"
+Entity = class:new()
+
+Entity.x = 1
+Entity.y = 1
+Entity.id = "entity"
+Entity.type = "entities"
+Entity.health = 100
+Entity.maxHealth = 100
+Entity.xVelocity = 0
+Entity.yVelocity = 0
+Entity.entitySpeed = 8
+Entity.entityAcceleration = 4
+Entity.friction = 0.8
+Entity.shadow = {}
+Entity.hostile = false
+Entity.animFrames = 4
+
+function Entity:load()
+    self:loadAnim()
+end
+
+function Entity:loadAnim()
+    self.texture = loadImage(self.type, self.id)
+    if self.type == "entities" then
+        self.animations = {
+            idle = nim.newAnim(self.texture, self.texture:getWidth()/self.animFrames, self.texture:getHeight()/2, 1, 1),
+            running = nim.newAnim(self.texture, self.texture:getWidth()/self.animFrames, self.texture:getHeight()/2, 1, 2)
+        }
+    else
+        self.animations = {
+            idle = nim.newAnim(self.texture, 32, 32, 1, 1),
+        }
+    end
+    self.currentAnimation = self.animations.idle
+end
+function Entity:loadShadow()
+	if #self.shadow > 0 then
+		if self.shadowIndex then
+			error("You can't load multiple shadows for one entity.")
+		else
+			local vertices = {}
+			for i, v in ipairs(self.shadow) do
+				table.insert(vertices, self.x * 32 + v.x)
+				table.insert(vertices, self.y * 32 + v.y)
+			end
+			table.insert(worldShadows, PolygonShadow:new(shadowshapes, unpack(vertices)))
+			self.shadowIndex = #worldShadows
+		end
+	end
+end
+function Entity:unloadShadow()
+	if self.shadowIndex then
+		worldShadows[self.shadowIndex]:Remove()
+		self.shadowIndex = nil
+	end
+end
+
+function Entity:hurt(dmg)
+    self.health = self.health - dmg
+end
+function Entity:heal(hp)
+    if self.health + hp > self.maxHealth then self.health = self.maxHealth else self.health = self.health + hp end
+end
+
+function Entity:moveDir(dt)
+    local mv = {}
+    mv.x = player.x - self.x
+    mv.y = player.y - self.y
+    local distance = math.sqrt(mv.x * mv.x + mv.y * mv.y)
+    if math.abs(distance) > 0.5 then
+        self.xVelocity = mv.x / distance * self.entitySpeed
+        self.yVelocity = mv.y / distance * self.entitySpeed
+    end
+end
+
+function Entity:move(dt)
+    self.xVelocity = self.xVelocity * self.friction
+    self.yVelocity = self.yVelocity * self.friction
+
+    if math.abs(self.xVelocity) < 0.001 then self.xVelocity = 0 end
+    if math.abs(self.yVelocity) < 0.001 then self.yVelocity = 0 end
+
+    local pushValue = 1/32
+    local mv = {}
+
+    mv.x = self.x + self.xVelocity * dt/2
+    mv.y = self.y + self.yVelocity * dt/2
+
+    mv.XtopLeft = tileMap[math.floor(mv.x + pushValue) .. " " .. math.floor(self.y + pushValue + 0.5)]
+    mv.XbotLeft = tileMap[math.floor(mv.x + pushValue) .. " " .. ceiling (self.y - pushValue)]
+    mv.XtopRight = tileMap[ceiling(mv.x - pushValue) .. " " .. math.floor(self.y + pushValue + 0.5)]
+    mv.XbotRight = tileMap[ceiling(mv.x - pushValue) .. " " .. ceiling (self.y - pushValue)]
+
+    if (self.x > mv.x) and (mv.XtopLeft ~= nil and not mv.XtopLeft.solid) and (mv.XbotLeft ~= nil and not mv.XbotLeft.solid) or --mv Left
+       (self.x < mv.x) and (mv.XtopRight ~= nil and not mv.XtopRight.solid) and (mv.XbotRight ~= nil and not mv.XbotRight.solid) then --mv Right
+           self.x = mv.x
+    end
+
+    mv.YtopLeft = tileMap[math.floor(self.x + pushValue) .. " " .. math.floor(mv.y + pushValue + 0.5)]
+    mv.YtopRight = tileMap[ceiling(self.x - pushValue) .. " " .. math.floor(mv.y + pushValue + 0.5)]
+    mv.YbotLeft = tileMap[math.floor(self.x + pushValue) .. " " .. ceiling (mv.y - pushValue)]
+    mv.YbotRight = tileMap[ceiling(self.x - pushValue) .. " " .. ceiling (mv.y - pushValue)]
+
+    if (self.y < mv.y) and (mv.YbotLeft ~= nil and not mv.YbotLeft.solid) and (mv.YbotRight ~= nil and not mv.YbotRight.solid) or --mv Down
+       (self.y > mv.y) and (mv.YtopLeft ~= nil and not mv.YtopLeft.solid) and (mv.YtopRight ~= nil and not mv.YtopRight.solid) then --mv Up
+           self.y = mv.y
+    end
+
+end
+
+function Entity:checkHit(proj)
+    return math.floor(self.x) <= math.floor(proj.x/32) and math.floor(self.y) <= math.floor(proj.y/32) and
+            ceiling(self.x + 0.5) >=  ceiling(proj.x/32) and  ceiling(self.y + 0.5) >=  ceiling(proj.y/32)
+end
+
+function Entity:update(dt)
+    self:moveDir(dt)
+    self:move(dt)
+	self.currentAnimation:update(dt)
+end
---
+++ main.lua
@@ -0,0 +1,208 @@
+require "globals"
+require "utils"
+require "tiles"
+require "item"
+require "weapon"
+require "ranged"
+require "melee"
+require "factories"
+require "inventory"
+require "entity"
+require "prop"
+require "player"
+require "mapGenerator"
+require "drawables"
+require "commands"
+require "projectile"
+
+lightworld:SetColor(127, 127, 127)
+
+function love.load()
+    player = Player()
+    player:load()
+    --math.randomseed(os.clock())
+    math.randomseed(1337)
+    --generateMap()
+    randWalkMap(1337)
+    fillHolesAndSetWalls()
+    generateStringMap()
+    addProps()
+	generateMapLighting()
+    --exampleMap()
+	atlasBatch = love.graphics.newSpriteBatch(generateAtlas(), 2000)
+	fillSpriteBatch(atlasBatch, tileMap)
+	randWalkSpawn(player, 20)
+    for entity = 0, 10 do
+		local zombie = entityFactory("zombie")
+		local skelebomber = entityFactory("skelebomber")
+		zombie:load()
+		skelebomber:load()
+		randWalkSpawn(zombie, 20)
+		randWalkSpawn(skelebomber, 20)
+		table.insert(entities, zombie)
+		table.insert(entities, skelebomber)
+    end
+	initMenu()
+end
+
+function love.update(dt)
+    player:update(dt)
+    for k, v in pairs(entities) do
+        v:update(dt)
+        for k2, proj in pairs(projectiles) do
+            if v:checkHit(proj) then
+                local dmg = 0
+                if math.random(proj.critChance) == 1 then
+                    dmg = (math.random(proj.minDmg, proj.maxDmg) * proj.critMultiplier)
+                else
+                    dmg = (math.random(proj.minDmg, proj.maxDmg))
+                end
+                v:hurt(dmg)
+                proj = nil
+                projectiles[k2] = nil
+            end
+        end
+        if v.health <= 0 then
+			entities[k]:unloadShadow()
+            entities[k] = nil
+        end
+    end
+	lightworld:SetPosition(math.floor((player.x + 0.5) * 32 + (-love.graphics.getWidth() / 2)), math.floor(player.y * 32 + (-love.graphics.getHeight() / 2)), 1)
+	playerLight:SetPosition((player.x * 32) + 16, (player.y * 32) + 24)
+	lightworld:Update()
+	updateSliders()
+    if player.inventory.hotbar[player.inventory.selected] ~= "none" then
+        player.inventory.hotbar[player.inventory.selected].cooldownTimer = math.max(player.inventory.hotbar[player.inventory.selected].cooldownTimer - dt, 0)
+    end
+	if player.rolling then
+		player.rollTime = player.rollTime + dt
+		player:updateRoll()
+		if player.rollTime > player.rollLength then
+			player.rolling = false
+		end
+	else
+		player.rollCooldown = math.max(player.rollCooldown - dt, 0)
+	end
+	nim.updateParticles(dt)
+end
+
+function love.textinput(t)
+    if player.chatOpen then
+        local tmp = love.graphics.newText(love.graphics.newFont(16), player.chatWrite .. t)
+        if tmp:getWidth() < 320 then
+            player.chatWrite = player.chatWrite .. t
+        end
+    end
+end
+
+function love.keypressed(key, scancode, isrepeat)
+    if player.chatOpen == true then
+        if key == "return" or key == "kpenter" then
+            player.chatOpen = false
+            if string.sub(player.chatWrite, 1, 1) == "/" and string.len(player.chatWrite) > 1 then evalCommands(player.chatWrite) end
+            if len(chatTexts) >= 22 then
+                table.remove(chatTexts, 1)
+            end
+            table.insert(chatTexts, player.chatWrite)
+            player.chatWrite = ""
+            return
+        end
+
+        tmp = love.graphics.newText(love.graphics.newFont(16), player.chatWrite .. love.system.getClipboardText())
+        if love.keyboard.isDown("lctrl") and key == "v" and tmp:getWidth() < 320 then
+            player.chatWrite = player.chatWrite .. love.system.getClipboardText()
+        elseif key == "backspace" then player.chatWrite = string.sub(player.chatWrite, 1, string.len(player.chatWrite)-1) end
+    elseif key == player.controls["debug"] then player.debugMode = not player.debugMode end
+    if (key == "return" or key == "kpenter") and player.chatOpen == false then
+        player.chatOpen = true
+    end
+    if inList({ "1", "2", "3", "4", "0" }, key) then
+        if key == "0" then key = "4" end
+		if tonumber(key) ~= player.inventory.selected then
+			if player.inventory.hotbar[player.inventory.selected].id ~= nil then player.itemCooldown = math.max(player.inventory.hotbar[player.inventory.selected].cooldown, 1) end
+		end
+        player.inventory.selected = tonumber(key)
+    end
+    if key == "/" and player.chatOpen == false then
+        player.chatOpen = true
+    end
+
+end
+
+function love.wheelmoved(x, y)
+    if y < 0 then
+        if player.inventory.selected < #player.inventory.hotbar then
+            player.inventory.selected = player.inventory.selected + 1
+        else player.inventory.selected = 1
+        end
+		if player.inventory.hotbar[player.inventory.selected].id ~= nil then player.itemCooldown = math.max(player.inventory.hotbar[player.inventory.selected].cooldown, 1) end
+    end
+    if y > 0 then
+        if player.inventory.selected > 1 then
+            player.inventory.selected = player.inventory.selected - 1
+        else player.inventory.selected = #player.inventory.hotbar
+        end
+		if player.inventory.hotbar[player.inventory.selected].id ~= nil then player.itemCooldown = math.max(player.inventory.hotbar[player.inventory.selected].cooldown, 1) end
+    end
+end
+
+function love.mousepressed(x, y, button, isTouch)
+    if button == 1 and player.inventory.hotbar[player.inventory.selected].cooldownTimer == 0 and not player.rolling then
+        if player.inventory.hotbar[player.inventory.selected].type == "ranged" then
+            player.inventory.hotbar[player.inventory.selected]:leftClick(x, y)
+        end
+    end
+end
+
+function love.draw()
+    love.graphics.reset()
+    love.graphics.push()
+
+
+    love.graphics.setBackgroundColor(0, 0, 0)
+    love.graphics.translate(-(player.x + 0.5) * 32 + (love.graphics.getWidth() / 2), -player.y * 32 + (love.graphics.getHeight() / 2))
+
+	--[[
+    for k, v in pairs(tileMap) do
+        local coords = split(k, " ")
+        love.graphics.draw(v.texture, coords[1] * 32, coords[2] * 32)
+    end
+	--]]
+	love.graphics.draw(atlasBatch)
+	nim.drawParticles()
+	for k, v in pairs(entities) do
+        nim.drawAnim(v.currentAnimation, v.x * 32, v.y * 32)
+    end
+
+	updateProjectiles()
+
+    love.graphics.reset()
+	lightworld:Draw()
+
+	love.graphics.translate(math.floor(-(player.x + 0.5) * 32 + (love.graphics.getWidth() / 2)), math.floor(-player.y * 32 + (love.graphics.getHeight() / 2)))
+	local flip = player.facing == "left"
+    nim.drawAnim(player.currentAnimation, player.x * 32, (player.y - 1) * 32, 90, flip)
+	if player.inventory.hotbar[player.inventory.selected].id ~= nil then
+        local opp = love.mouse.getY() - love.graphics.getHeight()/2 - player.y
+        local adj = love.mouse.getX() - love.graphics.getWidth()/2 - player.x
+        local hyp = math.sqrt(opp * opp + adj * adj)
+        local f = 1
+        if flip then f = -1 end
+        local gunPos = 48
+        if f == -1 then gunPos = -8 end
+        love.graphics.draw(player.inventory.hotbar[player.inventory.selected].texture, player.x * 32 + gunPos, player.y * 32 + 16, f*math.asin(opp / hyp), f, 1, 16, 16)
+    end
+	love.graphics.reset()
+
+    love.graphics.draw(player.healthBar, love.graphics.getWidth()/2 - 48, love.graphics.getHeight() - 96 - 32)
+    local HPPerc = (1 - (player.health / player.maxHealth)) * 96
+    local HPQuad = love.graphics.newQuad(0, HPPerc, 96, 96, player.healthImage:getDimensions())
+    love.graphics.draw(player.healthImage, HPQuad, love.graphics.getWidth()/2 - 48, love.graphics.getHeight() - 96 - 32 + HPPerc)
+    player.inventory.draw()
+
+    drawWholeChat()
+    if player.chatOpen == true then drawChat() end
+    if player.debugMode then drawDebug() end
+
+    love.graphics.pop()
+end
