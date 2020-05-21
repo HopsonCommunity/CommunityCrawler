6b990d0b7a47b72eebdd65f41139fdf5f06eb559
IsLilyYaGirl <35901613+IsLilyYaGirl@users.noreply.github.com>
2020-05-21 16:33:59 +0100

    nim update

--- src/libs/nim.lua
+++ src/libs/nim.lua
@@ -169,8 +169,9 @@ function nim.drawParticles()
 	love.graphics.setColor(oc)
 end
 
-function nim.newTilemap(file, loader)
+function nim.loadTilemap(tm, file, loader)
 	local map = {}
+	tm.tiles = {}
 	if (loader == nil) or (loader == "csv") then
 		for line in love.filesystem.lines(fil) do
 			table.insert(map, line)
@@ -179,6 +180,161 @@ function nim.newTilemap(file, loader)
 			map[i] = split(map[i], ",")
 		end
 	end
+	for x, s in ipairs(map) do
+		for y, tile in ipairs(s) do
+			tm:set(x, y, tile)
+		end
+	end
+end
+
+function nim.newTaggedTile(name, tag)
+	local tile = {name = name, tag = tag}
+	return tile
+end
+
+function nim.newDataTile(name, data, tag)
+	local tile = {name = name, data = data, tag = tag}
+	return tile
+end
+
+function nim.newTilemap(tilegfx, size)
+	local map = {tiles = {}, tilegfx = tilegfx or {}, size = size or 16}
+	function map:set(x, y, v, layer)
+		local l = layer or 1
+		if not map.tiles[l] then map.tiles[l] = {} end
+		if map.tiles[l][x] then
+			map.tiles[l][x][y] = v
+		else
+			map.tiles[l][x] = {}
+			map.tiles[l][x][y] = v
+		end
+	end
+	function map:get(x, y, layer)
+		local l = layer or 1
+		if map.tiles[l][x] then
+			return map.tiles[l][x][y]
+		else
+			return nil
+		end
+	end
+	return map
+end
+
+function nim.drawTilemap(map, px, py)
+	local x, y = px or 0, py or 0
+	for l, layer in pairs(map.tiles) do
+		for _x, s in pairs(layer) do
+			for _y, tile in pairs(s) do
+				if (type(tile) == "string") or (type(tile) == "number") then
+					if map.tilegfx[tile] then
+						love.graphics.draw(map.tilegfx[tile], x + ((_x - 1) * map.size), y + ((_y - 1) * map.size))
+					else
+						love.graphics.draw(map.tilegfx.notex, x + ((_x - 1) * map.size), y + ((_y - 1) * map.size))
+					end
+				else
+					if type(map.tilegfx[tile.name]) == "table" then
+						if map.tilegfx[tile.name][tile.tag] then
+							love.graphics.draw(map.tilegfx[tile.name][tile.tag], x + ((_x - 1) * map.size), y + ((_y - 1) * map.size))
+						else
+							love.graphics.draw(map.tilegfx.notex, x + ((_x - 1) * map.size), y + ((_y - 1) * map.size))
+						end
+					else
+						if map.tilegfx[tile.name] then
+							love.graphics.draw(map.tilegfx[tile.name], x + ((_x - 1) * map.size), y + ((_y - 1) * map.size))
+						else
+							love.graphics.draw(map.tilegfx.notex, x + ((_x - 1) * map.size), y + ((_y - 1) * map.size))
+						end
+					end
+				end
+			end
+		end
+	end
+end
+
+function nim.drawTilemapLayer(map, dl, px, py)
+	local x, y = px or 0, py or 0
+	for _x, s in pairs(map.tiles[dl]) do
+		for _y, tile in pairs(s) do
+			if (type(tile) == "string") or (type(tile) == "number") then
+				if map.tilegfx[tile] then
+					love.graphics.draw(map.tilegfx[tile], x + ((_x - 1) * map.size), y + ((_y - 1) * map.size))
+				else
+					love.graphics.draw(map.tilegfx.notex, x + ((_x - 1) * map.size), y + ((_y - 1) * map.size))
+				end
+			else
+				if type(map.tilegfx[tile.name]) == "table" then
+					if map.tilegfx[tile.name][tile.tag] then
+						love.graphics.draw(map.tilegfx[tile.name][tile.tag], x + ((_x - 1) * map.size), y + ((_y - 1) * map.size))
+					else
+						love.graphics.draw(map.tilegfx.notex, x + ((_x - 1) * map.size), y + ((_y - 1) * map.size))
+					end
+				else
+					if map.tilegfx[tile.name] then
+						love.graphics.draw(map.tilegfx[tile.name], x + ((_x - 1) * map.size), y + ((_y - 1) * map.size))
+					else
+						love.graphics.draw(map.tilegfx.notex, x + ((_x - 1) * map.size), y + ((_y - 1) * map.size))
+					end
+				end
+			end
+		end
+	end
+end
+
+function nim.drawTilemapNotLayer(map, nl, px, py)
+	local x, y = px or 0, py or 0
+	for l, layer in pairs(map.tiles) do
+		if l ~= nl then
+			for _x, s in pairs(layer) do
+				for _y, tile in pairs(s) do
+					if (type(tile) == "string") or (type(tile) == "number") then
+						if map.tilegfx[tile] then
+							love.graphics.draw(map.tilegfx[tile], x + ((_x - 1) * map.size), y + ((_y - 1) * map.size))
+						else
+							love.graphics.draw(map.tilegfx.notex, x + ((_x - 1) * map.size), y + ((_y - 1) * map.size))
+						end
+					else
+						if type(map.tilegfx[tile.name]) == "table" then
+							if map.tilegfx[tile.name][tile.tag] then
+								love.graphics.draw(map.tilegfx[tile.name][tile.tag], x + ((_x - 1) * map.size), y + ((_y - 1) * map.size))
+							else
+								love.graphics.draw(map.tilegfx.notex, x + ((_x - 1) * map.size), y + ((_y - 1) * map.size))
+							end
+						else
+							if map.tilegfx[tile.name] then
+								love.graphics.draw(map.tilegfx[tile.name], x + ((_x - 1) * map.size), y + ((_y - 1) * map.size))
+							else
+								love.graphics.draw(map.tilegfx.notex, x + ((_x - 1) * map.size), y + ((_y - 1) * map.size))
+							end
+						end
+					end
+				end
+			end
+		end
+	end
+end
+
+function nim.newProgressBar(sx, sy, typ, f, c, bc, oc)
+	local bar = {sx = sx, sy = sy, typ = (typ or "horizontal"), c = c or {0.2, 1, 0.4}, bc = bc or {((c or {})[1] or 0.2) * 0.3, ((c or {})[2] or 1) * 0.3, ((c or {})[3] or 0.4) * 0.3, 0.7}, oc = oc or {((c or {})[1] or 0.2) * 0.5, ((c or {})[2] or 1) * 0.5, ((c or {})[3] or 0.4) * 0.5, 0.7}, f = f or 0}
+	return bar
+end
+
+function nim.drawProgressBar(bar, x, y)
+	local c = {love.graphics.getColor()}
+	love.graphics.setColor(bar.oc)
+	love.graphics.rectangle("line", x, y, bar.sx, bar.sy)
+	love.graphics.setColor(bar.bc)
+	love.graphics.rectangle("fill", x + 1, y + 1, bar.sx - 2, bar.sy - 2)
+	love.graphics.setColor(bar.c)
+	if bar.typ == "horizontal" then
+		if bar.f ~= 0 then
+			love.graphics.rectangle("fill", x + 1, y + 1, (bar.sx*(bar.f/100)) - 2, bar.sy - 2)
+		end
+	elseif bar.typ == "vertical" then
+		if bar.f ~= 0 then
+			love.graphics.rectangle("fill", x + 1, y + 1, bar.sx - 2, (bar.sy*(bar.f/100)) - 2)
+		end
+	end
+	love.graphics.setColor(c)
 end
 
 return nim
